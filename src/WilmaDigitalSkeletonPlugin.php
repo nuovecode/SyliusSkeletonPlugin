7f62442890ec27e27e5e6dbca3866bfe44632165
irene <irene.iaccio@gmail.com>
2025-02-21 11:38:35 +0100

    Update bundle

--- src/WilmaDigitalSkeletonPlugin.php
+++ src/WilmaDigitalSkeletonPlugin.php
@@ -9,4 +9,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 final class WilmaDigitalSkeletonPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
